32bc3bb1747d439ef4a2330ef32f2a7ffb1bda2e
RAF-Universal <builder2011r@gmail.com>
2024-10-05 23:55:57 +0200

    Delete display_webhook.bat

--- display_webhook.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo [31mh[33mt[32mt[36mp[34ms[35m:[31m/[33m/[32md[36mi[34ms[35mc[31mo[33mr[32md[36m.[34mc[35mo[31mm[33m/[32ma[36mp[34mi[35m/[31mw[33me[32mb[36mh[34mo[35mo[31mk[33ms[32m/[36m1[34m2[35m9[31m1[33m8[32m0[36m3[34m9[35m8[31m9[33m1[32m9[36m2[34m6[35m0[31m9[33m8[32m7[36m4[34m/[35ml[31m1[33mc[32ms[36m7[34mN[35mF[31mM[33mi[32mm[36mb[34m1[35mU[31mL[33mB[32mT[36m7[34m7[35m6[31mh[33mo[32m_[36m-[34mB[35mn[31mQ[33mP[32mA[36mj[34mD[35m-[31mM[33mN[32mC[36mZ[34mh[35mE[31mj[33mN[32mC[36mC[34mo[35mi[31mc[33mw[32m0[36mH[34m4[35mI[31mJ[33mB[32mu[36mw[34m2[35m1[31mf[33mW[32mf[36mf[34mo[35mu[31mh[33mL[32mO[36mJ[34mp[35md[31mt
-echo.
-echo Cliquez sur le lien ci-dessus et appuyez sur une touche pour copier l'URL.
-pause
-echo Message Copied
